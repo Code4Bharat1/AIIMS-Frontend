c135b5e33ed0559214c9d8e88839ebbc6f390834
Abhi <mrunmaykanekar@gmail.com>
2025-05-01 09:54:31 +0530

    Instructor data are uploaded

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import { RiFacebookFill } from "react-icons/ri";
+import { IoLogoInstagram, IoMail } from "react-icons/io5";
+import { FaYoutube, FaPhoneAlt } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#181d38] text-white py-8 px-4 md:px-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="flex justify-center items-center md:justify-start">
+          <Image
+            src="/Navbar&Footer/AIMLOGO.png"
+            alt="AIM Tutorials Logo"
+            width={332}
+            height={120}
+            className="object-cover object-center w-[332px] h-[120px]"
+          />
+        </div>
+
+        <div className="sm:mx-auto md:mx-0">
+          <h3 className="text-lg font-semibold mb-2">Quick Link</h3>
+          <ul className="space-y-1 text-sm">
+            {["About Us", "Results", "Gallery", "Contact Us"].map(
+              (item, index) => (
+                <li key={index} className="flex items-center gap-1">
+                  <IoIosArrowForward className="text-blue-400" />
+                  <Link href="#" className="hover:text-blue-400">
+                    {item}
+                  </Link>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-semibold mb-2">Contact</h3>
+          <p className="text-sm flex items-start gap-2">
+            <FaLocationDot className="text-xl mt-1" />
+            19, Sunil Sadan, 3rd Floor, Central Avenue Road, Near Ambedkar
+            Garden, Opp. of Grand Central hotel, Above Monginis OR Yewle Tea,
+            Chembur(East)-71
+          </p>
+          <p className="mt-2 text-sm flex items-center gap-2">
+            <FaPhoneAlt />
+            +91 9870780524 / +91 8779955027
+          </p>
+          <p className="mt-1 text-sm flex items-center gap-2 hover:text-blue-400 cursor-pointer">
+            <IoMail />
+            ghodke.kishor@yahoo.in
+          </p>
+          <div className="flex space-x-2 mt-3">
+            {[IoLogoInstagram, RiFacebookFill, FaYoutube].map((Icon, i) => (
+              <div key={i} className="border border-white rounded-full p-1">
+                <Icon />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-semibold mb-2">About Us</h3>
+          <p className="text-sm">
+            At AIM Tutorials, we offer advanced classrooms, parent-teacher
+            meetings, top-notch teacher, and a leading curriculum. Experience
+            the best in education with us.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/utils/data.js
@@ -0,0 +1,46 @@
+export const instructorDetails = [
+    {
+        name: "Hrishi More",
+        education: "Biology 11 + 12 (NEET/MHT-CET)",
+        image: "/instructor/Hrishi.jpg",
+        social: {
+            facebook: "https://facebook.com/",
+            youtube: "https://youtube.com/",
+            instagram: "https://instagram.com/",
+            linkedin: "https://linkedin.com/",
+        },
+    },
+    {
+        name: "Kishor Ghodke",
+        education: "Qualication- MSC - BEd (Maths)",
+        image: "/instructor/Kishor.jpg",
+        social: {
+            facebook: "https://facebook.com/",
+            youtube: "https://youtube.com/",
+            instagram: "https://instagram.com/",
+            linkedin: "https://linkedin.com/",
+        },
+    },
+    {
+        name: "Shifa Khan",
+        education: "Biology 11+12(NEET)",
+        image: "/instructor/Sifa.jpg",
+        social: {
+            facebook: "https://facebook.com/",
+            youtube: "https://youtube.com/",
+            instagram: "https://instagram.com/",
+            linkedin: "https://linkedin.com/",
+        },
+    },
+    {
+        name: "SureshKumar Yadav",
+        education: "Maths 11 +12 (JEE/MHT-CET)",
+        image: "/instructor/Suresh.jpg",
+        social: {
+            facebook: "https://facebook.com/",
+            youtube: "https://youtube.com/",
+            instagram: "https://instagram.com/",
+            linkedin: "https://linkedin.com/",
+        },
+    },
+]
