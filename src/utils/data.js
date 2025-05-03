1c595a480efa83d0bc1ddd8dc2e444c34f016718
Vaibhav-stack31 <tiwariv1669@gmail.com>
2025-05-03 10:05:48 +0530

    Updated WhyChooseUs section

--- src/utils/data.js
+++ src/utils/data.js
@@ -9,14 +9,14 @@ export const whyChooseUs = [
   {
     imgPath: "/Book.png",
     title:
-      "All subjects under one roof",
+      "All subjects under • one roof",
     description:
       "All subjects are offered under one roof, providing a comprehensive and convenient learning experience. Students benefit from a seamless education journey without the need to seek external resources.",
   },
   {
     imgPath: "/Book.png",
     title:
-      "Subject wise best faculty in the industry",
+      "Subject wise best • faculty in the industry",
     description:
       "Each subject is taught by dedicated faculty experts, ensuring that students receive specialized and in-depth instruction. Our expert teachers bring a wealth of knowledge and experience to foster academic excellence.",
   },
@@ -25,7 +25,7 @@ export const whyChooseUs = [
     title:
       "Regular parents-teachers meeting",
     description:
-      "We hold regular parent-teacher meetings to keep parents informed and engaged in their child's academic progress. This open communication ensures a collaborative approach to each student's success.",
+      "We hold regular parent-teacher meetings to keep parents informed and engaged in their child's academic progress. This open communication ensures a collaborative approach to each • student's success.",
   },
 ];
 
@@ -69,167 +69,213 @@ export const galleryImgs = [
 ];
 
 export const instructorDetails = [
-    {
-        name: "Hrishi More",
-        education: "Biology 11 + 12 (NEET/MHT-CET)",
-        image: "/instructor/Hrishi.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  {
+    name: "Hrishi More",
+    education:
+      "Biology 11 + 12 (NEET/MHT-CET)",
+    image: "/instructor/Hrishi.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Kishor Ghodke",
-        education: "Qualication- MSC - BEd (Maths)",
-        image: "/instructor/Kishor.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Kishor Ghodke",
+    education:
+      "Qualication- MSC - BEd (Maths)",
+    image: "/instructor/Kishor.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Shifa Khan",
-        education: "Biology 11+12(NEET)",
-        image: "/instructor/Sifa.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Shifa Khan",
+    education: "Biology 11+12(NEET)",
+    image: "/instructor/Sifa.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "SureshKumar Yadav",
-        education: "Maths 11 +12 (JEE/MHT-CET)",
-        image: "/instructor/Suresh.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "SureshKumar Yadav",
+    education:
+      "Maths 11 +12 (JEE/MHT-CET)",
+    image: "/instructor/Suresh.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
+  },
 
-
-
-    {
-        name: "Sakeena Mam",
-        education: "Book keeping & Account 11+ 12(Commerce)",
-        image: "/instructor/Sakeena.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  {
+    name: "Sakeena Mam",
+    education:
+      "Book keeping & Account 11+ 12(Commerce)",
+    image: "/instructor/Sakeena.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Tanvi Khanna",
-        education: "ENGLISH 11+ 12( Science/Commerce)",
-        image: "/instructor/Tanvi.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Tanvi Khanna",
+    education:
+      "ENGLISH 11+ 12( Science/Commerce)",
+    image: "/instructor/Tanvi.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Vaibhav Gaikwad",
-        education: "SP 11+ 12(Commerce)",
-        image: "/instructor/Vaibhav.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Vaibhav Gaikwad",
+    education: "SP 11+ 12(Commerce)",
+    image: "/instructor/Vaibhav.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Ashfak A Shaikh",
-        education: "Physics 11 + 12 (NEET/MHT-CET/JEE)",
-        image: "/instructor/Ashfak.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Ashfak A Shaikh",
+    education:
+      "Physics 11 + 12 (NEET/MHT-CET/JEE)",
+    image: "/instructor/Ashfak.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Santosh Baliram Jadhav",
-        education: "Chemistry 11 + 12 (NEET/JEE/MHT-CET)",
-        image: "/instructor/Santosh.jpg",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Santosh Baliram Jadhav",
+    education:
+      "Chemistry 11 + 12 (NEET/JEE/MHT-CET)",
+    image: "/instructor/Santosh.jpg",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Yasmeen Khan",
-        education: "OCM 11+ 12(Commerce)",
-        image: "/instructor/Yasmeen.png",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Yasmeen Khan",
+    education: "OCM 11+ 12(Commerce)",
+    image: "/instructor/Yasmeen.png",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "Althaf Ahammed",
-        education: "Msc in Chemistry",
-        image: "/instructor/Althaf.png",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "Althaf Ahammed",
+    education: "Msc in Chemistry",
+    image: "/instructor/Althaf.png",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-    {
-        name: "SONAL GHODKE",
-        education: "Administrative Coordinator",
-        image: "/instructor/Sonal.png",
-        social: {
-            facebook: "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
-            youtube: "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
-            instagram: "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
-            linkedin: "https://linkedin.com/",
-        },
+  },
+  {
+    name: "SONAL GHODKE",
+    education:
+      "Administrative Coordinator",
+    image: "/instructor/Sonal.png",
+    social: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100091183034102&amp;mibextid=ZbWKwL",
+      youtube:
+        "https://youtube.com/@kishorghodke-xe6xn?si=DeE7pLXxbuq_kL2q",
+      instagram:
+        "https://www.instagram.com/_aim_tutorials?igsh=eGV0NGNlcmticngw",
+      linkedin: "https://linkedin.com/",
     },
-]
+  },
+];
 
 export const testimonialsData = [
-    {
-        imgPath: "/Testimonials/s1.png",
-        name: "Siddharth Talreja",
-        clg: "12 Sci - Swami College",
-        review: "Regular parent-teacher meetings keep my parents updated on my progress.",
-    },
-    {
-      imgPath: "/Testimonials/s2.png",
-        name: "Adnan Khan",
-        clg: "12 Sci - AFAC College",
-        review: "The technologically advanced classrooms make learning so much fun and effective!",
-    },
-    {
-      imgPath: "/Testimonials/s3.png",
-        name: "Shreya Mishra",
-        clg: "12 Sci - Somaiya College",
-        review: "The personal attention I receive here helps me understand everything better.",
-    },
+  {
+    imgPath: "/Testimonials/s1.png",
+    name: "Siddharth Talreja",
+    clg: "12 Sci - Swami College",
+    review:
+      "Regular parent-teacher meetings keep my parents updated on my progress.",
+  },
+  {
+    imgPath: "/Testimonials/s2.png",
+    name: "Adnan Khan",
+    clg: "12 Sci - AFAC College",
+    review:
+      "The technologically advanced classrooms make learning so much fun and effective!",
+  },
+  {
+    imgPath: "/Testimonials/s3.png",
+    name: "Shreya Mishra",
+    clg: "12 Sci - Somaiya College",
+    review:
+      "The personal attention I receive here helps me understand everything better.",
+  },
   {
     imgPath: "/Testimonials/s4.png",
     name: "Apurva Nikam",
     clg: "12 Com - AFAC College",
-    review: "The quality of education here is outstanding, and it shows in my grades!",
+    review:
+      "The quality of education here is outstanding, and it shows in my grades!",
   },
 ];
 
