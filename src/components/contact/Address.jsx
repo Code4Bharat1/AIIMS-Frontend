9fe79f34e48506237b3a0b7bf116ca7b067dbb5e
Abhi <mrunmaykanekar@gmail.com>
2025-05-03 12:27:13 +0530

    Fix layout issues in Address component for improved responsiveness

--- src/components/contact/Address.jsx
+++ src/components/contact/Address.jsx
@@ -43,7 +43,7 @@ const Address = () => {
     return (
       <div className="flex flex-col lg:flex-row justify-start relative mt-30 mb-30 px-4 lg:px-0">
         {/* Desktop layout with background */}
-        <div className="hidden lg:flex bg-[#AFDDFF] lg:h-[600px] lg:w-[800px] xl:w-[1100px] rounded-r-full relative z-10 items-center px-10 pr-60 text-2xl ">
+        <div className="hidden lg:flex bg-[#AFDDFF] h-[600px] lg:w-[700px] xl:w-[1100px] rounded-r-full relative z-10 items-center px-10 pr-60 text-2xl ">
           <div className="flex-1" />
           <ContactInfo />
         </div>
@@ -54,7 +54,7 @@ const Address = () => {
         </div>
 
         {/* Embedded Google Map */}
-        <div className="relative lg:absolute w-full lg:w-[450px] xl:w-[450px] lg:right-[calc(100%-1000px)] xl:right-[calc(100%-1300px)] lg:top-1/2 lg:-translate-y-1/2 h-[500px] bg-[#2467C9] z-20 rounded-2xl flex flex-col justify-center items-center lg:items-start gap-8 p-4 text-white overflow-hidden mx-auto lg:mx-0 select-none">
+        <div className="relative lg:absolute w-full lg:w-[450px] xl:w-[450px] lg:right-[calc(100%-900px)] xl:right-[calc(100%-1300px)] lg:top-1/2 lg:-translate-y-1/2 h-[500px] bg-[#2467C9] z-20 rounded-2xl flex flex-col justify-center items-center lg:items-start gap-8 p-4 text-white overflow-hidden mx-auto lg:mx-0 select-none">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7542.1080115727145!2d72.900999!3d19.061363!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c7bf91346769%3A0x540d723b75c1ec50!2sAIM%20TUTORIALS...%20Prof.kishor%20sir!5e0!3m2!1sen!2sin!4v1746089026216!5m2!1sen!2sin"
             width="100%"
